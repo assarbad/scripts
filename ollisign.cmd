035be003f4b034d4a93d306e5bed1748b63dead6
Oliver Schneider <oliver@assarbad.net>
2015-07-11 18:03:30 +0000

    Several adjustments for convenience

--- ollisign.cmd
+++ ollisign.cmd
@@ -8,18 +8,36 @@
 :::             held liable for any damages arising from the use of this
 :::             software.
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+setlocal ENABLEEXTENSIONS
+:: Look in current directory
+set SIGNTOOL=%~dp0signtool.exe
+set AC= /ac %~dp0MSCV-VSClass3.cer
+:: Check whether the caller wants verbose signtool.exe output
+if "%~1" == "-v"        shift&set VERBOSE=1
+if "%~1" == "--verbose" shift&set VERBOSE=1
+if "%~1" == "/v"        shift&set VERBOSE=1
+if "%~1" == "/verbose"  shift&set VERBOSE=1
 if "%~1" == "" goto :NoFileToSign
-call setvcvars.cmd > NUL 2>&1
+:: Check if that succeeds and look for Windows 8.1 SDK (x64) otherwise
+signtool /? >NUL 2>NUL || set SIGNTOOL=%ProgramFiles(x86)%\Windows Kits\8.1\bin\x64\signtool.exe
+:: Check if that succeeds and look for Windows 8.1 SDK (x86) otherwise
+signtool /? >NUL 2>NUL || set SIGNTOOL=%ProgramFiles(x86)%\Windows Kits\8.1\bin\x86\signtool.exe
+:: Otherwise look for the newest Visual Studio we can get
+signtool /? >NUL 2>NUL || call setvcvars.cmd > NUL 2>&1
+if not "%VCVER_FRIENDLY%" == "" @(
+  echo Using %VCVER_FRIENDLY%
+)
 set TIMESTAMP=/t "http://timestamp.verisign.com/scripts/timstamp.dll"
-set IDENTIFIER=/sm /r VeriSign /ac %~dp0\MSCV-VSClass3.cer
+set IDENTIFIER=/i Symantec%AC%
 if not "%~2" == "" @(
   call :SetVar DESCRIPTURL "%~2"
 )
 if not "%~3" == "" @(
   call :SetVar DESCRIPTION "%~3"
 )
-set VRFYCMD=signtool.exe verify /pa "%~1"
-set SIGNCMD=signtool.exe sign /a %IDENTIFIER% /ph
+set VRFYCMD=%SIGNTOOL% verify /pa "%~1"
+set SIGNCMD=%SIGNTOOL% sign /a %IDENTIFIER% /ph
+if not "%VERBOSE%" == "" set SIGNCMD=%SIGNCMD% /v /debug
 if not "%DESCRIPTURL%" == "" set SIGNCMD=%SIGNCMD% /du "%DESCRIPTURL%"
 if not "%DESCRIPTION%" == "" set SIGNCMD=%SIGNCMD% /d "%DESCRIPTION%"
 set SIGNCMD=%SIGNCMD% %TIMESTAMP% "%~1"
@@ -28,7 +46,7 @@ echo %SIGNCMD%
 %SIGNCMD%
 :: And verify
 %VRFYCMD%
-goto :EOF
+endlocal&goto :EOF
 :NoFileToSign
 echo ERROR: Need to give a file to sign!
 echo.
