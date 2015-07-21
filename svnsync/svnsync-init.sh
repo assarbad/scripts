e1a6a87cf4f7874ab33eddf30bae1f1d517fee43
Oliver Schneider <oliver@assarbad.net>
2015-07-21 15:02:39 +0000

    Adding ability to pass arguments to svnsync

--- svnsync/svnsync-init.sh
+++ svnsync/svnsync-init.sh
@@ -3,12 +3,15 @@ LOCALNAME="$1"
 REMOTEURL="$2"
 function syntax_help
 {
-  echo -e "Syntax:\n\tsvnsync-init <local> <remote>"
+  echo -e "Syntax:\n\tsvnsync-init <local> <remote>\n"
+  echo -e "Use environment variable SVNSYNCARGS to pass more colon-separated"
+  echo -e "arguments to 'svnsync init' and 'svnsync sync' respectively."
   exit 1
 }
 
 [[ -n "$LOCALNAME" ]] || syntax_help
 [[ -n "$REMOTEURL" ]] || syntax_help
+[[ -n "$SVNSYNCARGS" ]] && SVNSYNCARGS="${SVNSYNCARGS//:/ }"
 
 # Set default absolute path if it isn't alreay absolute ...
 [[ "${LOCALNAME:0:1}" == "/" ]] || LOCALNAME="$HOME/ext-repos/SVNSYNC/$LOCALNAME"
@@ -23,6 +26,6 @@ echo "svnadmin create \"$LOCALNAME\""
 echo '#!/bin/sh'|tee "$LOCALNAME/hooks/pre-revprop-change" && chmod +x "$LOCALNAME/hooks/pre-revprop-change"
 echo "echo '#!/bin/sh'|tee \"$LOCALNAME/hooks/pre-revprop-change\" && chmod +x \"$LOCALNAME/hooks/pre-revprop-change\""
 svnsync init "file://$LOCALNAME" "$REMOTEURL"
-echo "svnsync init \"file://$LOCALNAME\" \"$REMOTEURL\""
+echo "svnsync $SVNSYNCARGS init \"file://$LOCALNAME\" \"$REMOTEURL\""
 svnsync sync "file://$LOCALNAME"
-echo "svnsync sync \"file://$LOCALNAME\""
+echo "svnsync $SVNSYNCARGS sync \"file://$LOCALNAME\""
