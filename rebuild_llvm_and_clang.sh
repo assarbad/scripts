ae8fc78da6355a220ad3cb23838451959e4daa84
Oliver Schneider <oliver@assarbad.net>
2015-08-30 16:10:28 +0000

    Updates for version 3.7 of Clang/LLVM

--- rebuild_llvm_and_clang.sh
+++ rebuild_llvm_and_clang.sh
@@ -1,48 +1,50 @@
 #!/usr/bin/env bash
 # http://llvm.org/docs/GettingStarted.html#compiling-the-llvm-suite-source-code
 # possible alternative: https://github.com/rsmmr/install-clang
+[[ -t 1 ]] && { cG="\e[1;32m"; cR="\e[1;31m"; cB="\e[1;34m"; cW="\e[1;37m"; cY="\e[1;33m"; cG_="\e[0;32m"; cR_="\e[0;31m"; cB_="\e[0;34m"; cW_="\e[0;37m"; cY_="\e[0;33m"; cZ="\e[0m"; export cR cG cB cY cW cR_ cG_ cB_ cY_ cW_ cZ; }
+[[ -n "$DEBUG" ]] && set -x
 MEANDMYSELF=${0##*/}
-LLVM_RELEASE="release_36"
+LLVM_RELEASE="release_37"
 LIBCXXRT_RELEASE="stable"
-MUSLLIBC_RELEASE="v1.1.6"
+MUSLLIBC_RELEASE="v1.1.11"
 BINUTILS_RELEASE="binutils-2_24"
 INSTALL_TO=${INSTALL_TO:-$HOME/bin/LLVM}
 TARGETS="x86,x86_64"
 BASEDIR="${BASEDIR:-$(pwd)}"
 let NOOPTIONAL=0
-for tool in tee tar bzip2 sha1sum git date make cp; do type $tool > /dev/null 2>&1 || { echo "ERROR: couldn't find '$tool' which is required by this script."; exit 1; }; done
+for tool in tee tar bzip2 sha1sum git date make cp; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
 
 function show_help
 {
 	echo -e "Syntax: $MEANDMYSELF [-h|-?] [-B] [-C|-c|-p|-r] [-g] [-i <install-dir>] [-O] [-t <targets>] [-v]"
-	echo -e "\t-h | -?"
+	echo -e "\t${cW}-h | -?${cZ}"
 	echo -e "\t  Show this help"
-	echo -e "\t-B"
+	echo -e "\t${cW}-B${cZ}"
 	echo -e "\t  Do not actually build anything, but check out and prepare build directory."
-	echo -e "\t-c"
+	echo -e "\t${cW}-c${cZ}"
 	echo -e "\t  Only check out updates from upstream, then exit."
-	echo -e "\t-C"
+	echo -e "\t${cW}-C${cZ}"
 	echo -e "\t  Do not check out from the upstream repository."
-	echo -e "\t-g"
+	echo -e "\t${cW}-g${cZ}"
 	echo -e "\t  Garbage collect after checkout."
-	echo -e "\t-G"
-	echo -e "\t  Garbage collect aggressively (git gc --aggressive) after checkout."
-	echo -e "\t-i"
-	echo -e "\t  Set the installation directory. Can also be done by setting INSTALL_TO."
-	echo -e "\t-O"
+	echo -e "\t${cW}-G${cZ}"
+	echo -e "\t  Garbage collect aggressively (${cW}git gc --aggressive${cZ}) after checkout."
+	echo -e "\t${cW}-i${cZ}"
+	echo -e "\t  Set the installation directory. Can also be done by setting ${cW}INSTALL_TO${cZ}."
+	echo -e "\t${cW}-O${cZ}"
 	echo -e "\t  Do not build 'optional' components (i.e. musl + binutils)."
-	echo -e "\t-p"
-	echo -e "\t  Same as -c but also packages the Git repos in a .tgz file."
-	echo -e "\t-r"
-	echo -e "\t  Revives the repositories previously packaged with -p. Implies -c."
-	echo -e "\t  NB: to be called from the directory which contains ${MEANDMYSELF}."
-	echo -e "\t-t <targets> (default=$TARGETS)"
+	echo -e "\t${cW}-p${cZ}"
+	echo -e "\t  Same as ${cW}-c${cZ} but also packages the Git repos in a .tgz file."
+	echo -e "\t${cW}-r${cZ}"
+	echo -e "\t  Revives the repositories previously packaged with ${cW}-p${cZ}. Implies ${cW}-c${cZ}."
+	echo -e "\t  ${cW}NB:${cZ} to be called from the directory which contains ${MEANDMYSELF}."
+	echo -e "\t${cW}-t${cZ} <targets> (default=$TARGETS)"
 	echo -e "\t  Specify the target architectures for LLVM/Clang (such as x86, x86_64 ...)."
-	echo -e "\t-v"
-	echo -e "\t  Be verbose about the actions (lines get leading '[DBG]' string)."
+	echo -e "\t${cW}-v${cZ}"
+	echo -e "\t  Be verbose about the actions (lines get leading '${cB}[DBG]${cZ}' string)."
 	echo ""
-	echo -e "NOTE: the main difference between -c and -B is that -B will remove an existing"
-	echo -e "      build directory and -c doesn't touch that at all."
+	echo -e "${cW}NOTE:${cZ} the main difference between ${cW}-c${cZ} and ${cW}-B${cZ} is that ${cW}-B${cZ} will remove an existing"
+	echo -e "      build directory and ${cW}-c${cZ} doesn't touch that at all."
 }
 
 function prepare_src
@@ -58,31 +60,31 @@ function prepare_src
 		[[ -d "$BASEDIR/$PRJNAME" ]] || $GITCLONE "$BASEDIR/$PRJNAME"
 		# Sanity check the clone
 		((VERBOSE)) && echo "[DBG:$PRJ] Verifying the clone exists now."
-		[[ -d "$BASEDIR/$PRJNAME/.git" ]] || { echo "ERROR: apparently we failed to clone $PRJNAME ($GITCLONE)."; exit 1; }
+		[[ -d "$BASEDIR/$PRJNAME/.git" ]] || { echo -e "${cR}ERROR:${cZ} apparently we failed to clone $PRJNAME ($GITCLONE)."; exit 1; }
 		# Set the Git stuff according to the docs
 		((VERBOSE)) && echo "[DBG:$PRJ] Setting branch.master.rebase to true."
-		( cd "$BASEDIR/$PRJNAME" && git config branch.master.rebase true ) || { echo "ERROR: could not set 'git config branch.master.rebase true' for $PRJNAME."; exit 1; }
+		( cd "$BASEDIR/$PRJNAME" && git config branch.master.rebase true ) || { echo -e "${cR}ERROR:${cZ} could not set 'git config branch.master.rebase true' for $PRJNAME."; exit 1; }
 		( cd "$BASEDIR/$PRJNAME" && echo -n "$PRJ: branch.master.rebase = " && git config --get branch.master.rebase )
-		( cd "$BASEDIR/$PRJNAME" && if [[ "xtrue" == "x$(git config --get core.bare)" ]]; then git config --bool core.bare false; fi ) || { echo "ERROR: could not set 'git config --bool core.bare false' for $PRJNAME."; exit 1; }
+		( cd "$BASEDIR/$PRJNAME" && if [[ "xtrue" == "x$(git config --get core.bare)" ]]; then git config --bool core.bare false; fi ) || { echo -e "${cR}ERROR:${cZ} could not set 'git config --bool core.bare false' for $PRJNAME."; exit 1; }
 		((REVIVEPKG)) && ( cd "$BASEDIR/$PRJNAME" && echo -ne "\tHard-resetting ($(git config --get core.bare)) after thawing it.\n\t-> "; git reset --hard )
 		# Scrub the working copy
 		((VERBOSE)) && echo "[DBG:$PRJ] Cleaning extraneous files from Git clone."
-		( cd "$BASEDIR/$PRJNAME" && git clean -d -f ) || { echo "ERROR: failed to 'git clean' $PRJNAME."; exit 1; }
+		( cd "$BASEDIR/$PRJNAME" && git clean -d -f ) || { echo -e "${cR}ERROR:${cZ} failed to 'git clean' $PRJNAME."; exit 1; }
 		# Get latest changes to the Git repo
 		((VERBOSE)) && echo "[DBG:$PRJ] Fetching updates from upstream."
-		( cd "$BASEDIR/$PRJNAME" && git fetch ) || { echo "WARNING: failed to 'git fetch' $PRJNAME."; }
+		( cd "$BASEDIR/$PRJNAME" && git fetch ) || { echo -e "${cY}WARNING:${cZ} failed to 'git fetch' $PRJNAME."; }
 		# Check out the release
 		((VERBOSE)) && echo "[DBG:$PRJ] Checking out the files on current branch."
-		( cd "$BASEDIR/$PRJNAME" && echo -n "$PRJ: " && git checkout $GITREF ) || { echo "ERROR: failed to check out $GITREF for $PRJNAME."; exit 1; }
+		( cd "$BASEDIR/$PRJNAME" && echo -n "$PRJ: " && git checkout $GITREF ) || { echo -e "${cR}ERROR:${cZ} failed to check out $GITREF for $PRJNAME."; exit 1; }
 		if git --git-dir="$BASEDIR/$PRJNAME/.git" rev-parse --symbolic --branches|grep -q "$GITREF"; then
 			((VERBOSE)) && echo "[DBG:$PRJ] Fast-forwarding, if possible."
-			( cd "$BASEDIR/$PRJNAME" && echo -n "$PRJ: " && git merge --ff-only origin/$GITREF ) || { echo "ERROR: failed to fast-forward to origin/$GITREF for $PRJNAME."; exit 1; }
+			( cd "$BASEDIR/$PRJNAME" && echo -n "$PRJ: " && git merge --ff-only origin/$GITREF ) || { echo -e "${cR}ERROR:${cZ} failed to fast-forward to origin/$GITREF for $PRJNAME."; exit 1; }
 		fi
 		((GARBAGECOLLECT)) && ( echo "Now garbage-collecting the repository${GCAGGRESSIVE:+ ($GCAGGRESSIVE)}"; cd "$BASEDIR/$PRJNAME" && git gc $GCAGGRESSIVE --prune=all)
 	fi
 	((ONLYCHECKOUT)) && return
 	[[ -d "$BASEDIR/build/$PRJNAME" ]] && rm -rf "$BASEDIR/build/$PRJNAME"
-	mkdir -p "$BASEDIR/build/$PRJNAME" || { echo "ERROR: could not create build/$PRJNAME directory."; exit 1; }
+	mkdir -p "$BASEDIR/build/$PRJNAME" || { echo -e "${cR}ERROR:${cZ} could not create build/$PRJNAME directory."; exit 1; }
 }
 
 function show_time_diff
@@ -103,29 +105,29 @@ while getopts "h?BcCgGi:Oprt:v" opt; do
 		show_help
 		exit 0
 		;;
-	c)  ((NOCHECKOUT)) && { echo "ERROR: -C and -c/-p are mutually exclusive."; exit 1; }
+	c)  ((NOCHECKOUT)) && { echo -e "${cR}ERROR:${cZ} ${cY}-C${cZ} and ${cY}-c${cZ}/${cY}-p${cZ} are mutually exclusive."; exit 1; }
 		ONLYCHECKOUT=1
 		echo "Doing only a checkout"
 		;;
-	C)  ((ONLYCHECKOUT)) && { echo "ERROR: -C and -c/-p are mutually exclusive."; exit 1; }
+	C)  ((ONLYCHECKOUT)) && { echo -e "${cR}ERROR:${cZ} ${cY}-C${cZ} and ${cY}-c${cZ}/${cY}-p${cZ} are mutually exclusive."; exit 1; }
 		NOCHECKOUT=1
-		echo "Skipping checkout"
+		echo -e "${cY}Skipping checkout${cZ}"
 		;;
 	B)  NOBUILD=1
-		echo "Skipping build"
+		echo -e "${cY}Skipping build${cZ}"
 		;;
 	g)  GARBAGECOLLECT=1
 		;;
 	G)  GARBAGECOLLECT=1
 		GCAGGRESSIVE="--aggressive"
 		;;
-	i)  [[ -n "$OPTARG" ]] || { echo "ERROR: -$opt requires an argument." >&2; exit 1; }
+	i)  [[ -n "$OPTARG" ]] || { echo -e "${cR}ERROR:${cZ} ${cY}-$opt${cZ} requires an argument." >&2; exit 1; }
 		INSTALL_TO="$OPTARG"
 		;;
 	O)  NOOPTIONAL=1
-		echo "Skipping build of optional components"
+		echo -e "${cY}Skipping build of optional components${cZ}"
 		;;
-	p)  ((NOCHECKOUT)) && { echo "ERROR: -C and -p/-c are mutually exclusive."; exit 1; }
+	p)  ((NOCHECKOUT)) && { echo -e "${cR}ERROR:${cZ} ${cY}-C${cZ} and ${cY}-p${cZ}/${cY}-c${cZ} are mutually exclusive."; exit 1; }
 		ONLYCHECKOUT=1
 		PACKAGEGITGZ=1
 		echo "Doing only a checkout and then packaging bare clones into .tbz (requires tar+bzip2)."
@@ -135,23 +137,23 @@ while getopts "h?BcCgGi:Oprt:v" opt; do
 		echo "Reviving bare repos, followed a checkout."
 		;;
 	t)  [[ -n "$OPTARG" ]] && TARGETS="$OPTARG"
-		[[ -n "$OPTARG" ]] || { echo "ERROR: -$opt requires an argument." >&2; exit 1; }
+		[[ -n "$OPTARG" ]] || { echo -e "${cR}ERROR:${cZ} ${cY}-$opt${cZ} requires an argument." >&2; exit 1; }
 		;;
 	v)  VERBOSE=1
-		((VERBOSE)) && echo "[DBG] Enabling verbose run."
+		((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Enabling verbose run."
 		;;
 	esac
 done
 echo -e "\tWorking directory: $BASEDIR"
 let PM=$(grep -c processor /proc/cpuinfo)
-((VERBOSE)) && echo "[DBG] Number of parallel jobs, based on processor count: $PM."
+((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Number of parallel jobs, based on processor count: $PM."
 let TIME_START=$(date +%s)
-((VERBOSE)) && echo "[DBG] Preparing core projects."
+((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Preparing core projects."
 # Preparational steps
 for i in "llvm:git clone http://llvm.org/git/llvm.git" "llvm/tools/clang:git clone http://llvm.org/git/clang.git" "llvm/projects/compiler-rt:git clone http://llvm.org/git/compiler-rt.git" "llvm/projects/libcxx:git clone http://llvm.org/git/libcxx.git" "llvm/tools/clang/tools/extra:git clone http://llvm.org/git/clang-tools-extra.git"; do
 	prepare_src "$i" "$LLVM_RELEASE"
 done
-((VERBOSE)) && echo "[DBG] Preparing third-party projects."
+((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Preparing third-party projects."
 prepare_src "llvm/projects/libcxxrt:git clone https://github.com/pathscale/libcxxrt" "$LIBCXXRT_RELEASE"
 if ((NOOPTIONAL == 0)); then
 	prepare_src "3rdparty/musl:git clone git://git.musl-libc.org/musl" "$MUSLLIBC_RELEASE"
@@ -159,28 +161,28 @@ if ((NOOPTIONAL == 0)); then
 fi
 let TIME_GIT=$(date +%s)
 if ((ONLYCHECKOUT==0)) && ((NOBUILD==0)); then
-	((VERBOSE)) && echo "[DBG] Doing build for $TARGETS into $INSTALL_TO."
+	((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Doing build for $TARGETS into $INSTALL_TO."
 	if [[ -d "$BASEDIR/build/llvm" ]]; then
-		pushd "$BASEDIR/build/llvm" && \
-			( set -x; "$BASEDIR/llvm/configure" --prefix=$INSTALL_TO --disable-docs --enable-optimized --enable-targets=$TARGETS ) && \
-			let TIME_CONFIGURE=$(date +%s) && \
-			( set -x; make -j$PM ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 ) && \
-			let TIME_MAKE=$(date +%s) && \
-			( set -x; make -j$PM ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 install ) && \
-		popd
+		pushd "$BASEDIR/build/llvm" || { echo -e "${cR}ERROR:${cZ} could not ${cW}pushd${cZ}."; exit 1; }
+		( set -x; "$BASEDIR/llvm/configure" --prefix=$INSTALL_TO --disable-docs --enable-optimized --enable-targets=$TARGETS ) || { echo -e "${cR}ERROR:${cZ}  ${cW}./configure${cZ} failed."; exit 1; }
+		let TIME_CONFIGURE=$(date +%s)
+		( set -x; make -j$PM ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 )  || { echo -e "${cR}ERROR:${cZ}  ${cW}make${cZ} failed."; exit 1; }
+		let TIME_MAKE=$(date +%s)
+		( set -x; make -j$PM ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 install )  || { echo -e "${cR}ERROR:${cZ}  ${cW}make install${cZ} failed."; exit 1; }
+		popd || { echo -e "${cR}ERROR:${cZ} could not ${cY}popd${cZ}."; exit 1; }
 		for i in scan-view scan-build; do
-			cp -r "$BASEDIR/llvm/tools/clang/tools/$i"/*  "$INSTALL_TO/bin/"/ || { echo "WARNING: could not copy $i binaries/scripts."; }
+			cp -r "$BASEDIR/llvm/tools/clang/tools/$i"/*  "$INSTALL_TO/bin/"/ || { echo -e "${cY}WARNING:${cZ} could not copy $i binaries/scripts."; }
 		done
 		let TIME_INSTALL=$(date +%s)
 		if ((NOOPTIONAL == 0)); then
-			pushd "$BASEDIR/3rdparty/binutils" && \
-				( set -x; CC="$INSTALL_TO/bin/clang" ./configure --disable-werror --disable-gold --enable-ld --program-suffix=.clang --prefix=$INSTALL_TO ) && \
-				( set -x; make -j$PM && make install ) && \
-			popd
+			pushd "$BASEDIR/3rdparty/binutils" || { echo -e "${cR}ERROR:${cZ} could not ${cW}pushd${cZ}."; exit 1; }
+			( set -x; CC="$INSTALL_TO/bin/clang" ./configure --disable-werror --disable-gold --enable-ld --program-suffix=.clang --prefix=$INSTALL_TO ) || { echo -e "${cR}ERROR:${cZ} ${cY}./configure${cZ} (clang) failed."; exit 1; }
+			( set -x; make -j$PM && make install ) || { echo -e "${cR}ERROR:${cZ} ${cW}make${cZ} or ${cW}make install${cZ} (clang) failed."; exit 1; }
+			popd || { echo -e "${cR}ERROR:${cZ} could not ${cW}popd${cZ}."; exit 1; }
 		fi
 		let TIME_BINUTILS=$(date +%s)
 	else
-		echo "ERROR: no directory $BASEDIR/build/llvm."
+		echo -e "${cR}ERROR:${cZ} no directory $BASEDIR/build/llvm."
 		exit 1
 	fi
 fi
