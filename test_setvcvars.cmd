a44e8bc87ca9ba0a4f50530dce7ddfed92575306
Oliver Schneider <oliver@assarbad.net>
2015-11-04 20:40:51 +0000

    Making it possible to select the tests up front instead of throughout the script

--- test_setvcvars.cmd
+++ test_setvcvars.cmd
@@ -12,25 +12,35 @@
 :::             software.
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 cls
+set TESTSTORUN=HelpSwitches VS_nosuch VS2002 VS2003 VS2005 VS2008 VS2010 VS2012 VS2013 VS2015 VS2005_amd64 VS2008_amd64 VS2010_amd64 VS2012_amd64 VS2013_amd64 VS2015_amd64 VS2005_ia64 VS2008_ia64 VS2010_ia64 VS2012_ia64 VS2013_ia64 VS2015_ia64
+for %%i in (%TESTSTORUN%) do @(
+  @echo TEST %%i
+  prompt $G & call :%%i
+  prompt
+)
+@goto :EOF
 :: Check the help switches
+:HelpSwitches
 @setlocal&pushd .&echo on
-call %~dp0setvcvars.cmd -h
+call %~dp0setvcvars.cmd -h|findstr /l Syntax:
 @popd&endlocal
 @setlocal&pushd .&echo on
-call %~dp0setvcvars.cmd --help
+call %~dp0setvcvars.cmd --help|findstr /l Syntax:
 @popd&endlocal
 @setlocal&pushd .&echo on
-call %~dp0setvcvars.cmd /h
+call %~dp0setvcvars.cmd -?|findstr /l Syntax:
 @popd&endlocal
 @setlocal&pushd .&echo on
-call %~dp0setvcvars.cmd /help
+call %~dp0setvcvars.cmd /h|findstr /l Syntax:
 @popd&endlocal
 @setlocal&pushd .&echo on
-call %~dp0setvcvars.cmd -?
+call %~dp0setvcvars.cmd /help|findstr /l Syntax:
 @popd&endlocal
+@goto :EOF
 :: call /? will show the help for the call command, so we cannot reasonably test that
+::
 :: Test the known Visual Studio/C++ versions
-:: 2002
+:VS2002
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2002
 @popd&endlocal
@@ -40,7 +50,8 @@ call %~dp0setvcvars.cmd 2002
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 7.0
 @popd&endlocal
-:: 2003
+@goto :EOF
+:VS2003
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2003
 @popd&endlocal
@@ -50,7 +61,8 @@ call %~dp0setvcvars.cmd 2003
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 7.1
 @popd&endlocal
-:: 2005
+@goto :EOF
+:VS2005
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2005
 @popd&endlocal
@@ -60,7 +72,8 @@ call %~dp0setvcvars.cmd 2005
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 8.0
 @popd&endlocal
-:: 2008
+@goto :EOF
+:VS2008
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2008
 @popd&endlocal
@@ -70,7 +83,8 @@ call %~dp0setvcvars.cmd 2008
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 9.0
 @popd&endlocal
-:: 2010
+@goto :EOF
+:VS2010
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2010
 @popd&endlocal
@@ -80,7 +94,8 @@ call %~dp0setvcvars.cmd 2010
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 10.0
 @popd&endlocal
-:: 2012
+@goto :EOF
+:VS2012
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2012
 @popd&endlocal
@@ -90,7 +105,8 @@ call %~dp0setvcvars.cmd 2012
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 11.0
 @popd&endlocal
-:: 2013
+@goto :EOF
+:VS2013
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2013
 @popd&endlocal
@@ -100,7 +116,8 @@ call %~dp0setvcvars.cmd 2013
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 12.0
 @popd&endlocal
-:: 2015
+@goto :EOF
+:VS2015
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2015
 @popd&endlocal
@@ -110,8 +127,9 @@ call %~dp0setvcvars.cmd 2015
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 14.0
 @popd&endlocal
+@goto :EOF
 :: Some versions that don't exist
-:: 2001
+:VS_nosuch
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2001
 @popd&endlocal
@@ -130,8 +148,9 @@ call %~dp0setvcvars.cmd vs2300
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 98
 @popd&endlocal
+@goto :EOF
 :: Checking amd64 toolset on all
-:: 2005
+:VS2005_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2005
 @popd&endlocal
@@ -141,7 +160,8 @@ call %~dp0setvcvars.cmd amd64 2005
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 8.0
 @popd&endlocal
-:: 2008
+@goto :EOF
+:VS2008_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2008
 @popd&endlocal
@@ -151,7 +171,8 @@ call %~dp0setvcvars.cmd amd64 2008
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 9.0
 @popd&endlocal
-:: 2010
+@goto :EOF
+:VS2010_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2010
 @popd&endlocal
@@ -161,7 +182,8 @@ call %~dp0setvcvars.cmd amd64 2010
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 10.0
 @popd&endlocal
-:: 2012
+@goto :EOF
+:VS2012_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2012
 @popd&endlocal
@@ -171,7 +193,8 @@ call %~dp0setvcvars.cmd amd64 2012
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 11.0
 @popd&endlocal
-:: 2013
+@goto :EOF
+:VS2013_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2013
 @popd&endlocal
@@ -181,7 +204,8 @@ call %~dp0setvcvars.cmd amd64 2013
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 12.0
 @popd&endlocal
-:: 2015
+@goto :EOF
+:VS2015_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2015
 @popd&endlocal
@@ -191,8 +215,9 @@ call %~dp0setvcvars.cmd amd64 2015
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 14.0
 @popd&endlocal
+@goto :EOF
 :: Checking ia64 toolset on all
-:: 2005
+:VS2005_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2005
 @popd&endlocal
@@ -202,7 +227,8 @@ call %~dp0setvcvars.cmd ia64 2005
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 8.0
 @popd&endlocal
-:: 2008
+@goto :EOF
+:VS2008_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2008
 @popd&endlocal
@@ -212,7 +238,8 @@ call %~dp0setvcvars.cmd ia64 2008
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 9.0
 @popd&endlocal
-:: 2010
+@goto :EOF
+:VS2010_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2010
 @popd&endlocal
@@ -222,7 +249,8 @@ call %~dp0setvcvars.cmd ia64 2010
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 10.0
 @popd&endlocal
-:: 2012
+@goto :EOF
+:VS2012_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2012
 @popd&endlocal
@@ -232,7 +260,8 @@ call %~dp0setvcvars.cmd ia64 2012
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 11.0
 @popd&endlocal
-:: 2013
+@goto :EOF
+:VS2013_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2013
 @popd&endlocal
@@ -242,7 +271,8 @@ call %~dp0setvcvars.cmd ia64 2013
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 12.0
 @popd&endlocal
-:: 2015
+@goto :EOF
+:VS2015_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2015
 @popd&endlocal
@@ -252,3 +282,4 @@ call %~dp0setvcvars.cmd ia64 2015
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 14.0
 @popd&endlocal
+@goto :EOF
