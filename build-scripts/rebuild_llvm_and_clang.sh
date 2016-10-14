9444aefbc2b915b49184ffad201dbda9b698ace5
Oliver Schneider <oliver@assarbad.net>
2016-10-14 21:40:07 +0000

    Adjustments to Clang/LLVM build script

--- build-scripts/rebuild_llvm_and_clang.sh
+++ build-scripts/rebuild_llvm_and_clang.sh
@@ -4,23 +4,13 @@
 [[ -t 1 ]] && { cG="\e[1;32m"; cR="\e[1;31m"; cB="\e[1;34m"; cW="\e[1;37m"; cY="\e[1;33m"; cG_="\e[0;32m"; cR_="\e[0;31m"; cB_="\e[0;34m"; cW_="\e[0;37m"; cY_="\e[0;33m"; cZ="\e[0m"; export cR cG cB cY cW cR_ cG_ cB_ cY_ cW_ cZ; }
 ( [[ -n "$DEBUG" ]] || [[ -n "$DBG" ]] ) && { DBG=1; DEBUG=1; set -x; }
 MEANDMYSELF=${0##*/}
-LLVM_RELEASE="${LLVM_RELEASE:-release_36}"
-LIBCXXRT_RELEASE="${LIBCXXRT_RELEASE:-stable}"
-BINUTILS_RELEASE="${BINUTILS_RELEASE:-binutils-2_24}"
+LLVM_RELEASE="${LLVM_RELEASE:-release_39}"
+BINUTILS_RELEASE="${BINUTILS_RELEASE:-binutils-2_27}"
 INSTALL_TO=${INSTALL_TO:-$HOME/bin/LLVM}
 BASEDIR="${BASEDIR:-$(pwd)}"
+TARGETS="Native"
 let NOOPTIONAL=0
 for tool in tee tar bzip2 sha1sum git date make cp; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
-if ((${LLVM_RELEASE##*_} > 36)); then
-	echo -e "${cW}INFO:${cZ} preparing for build using CMake"
-	for tool in cmake; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
-	let CMAKE_BUILD=1
-	TARGETS="X86"
-else
-	let CMAKE_BUILD=0
-	TARGETS="x86,x86_64"
-	echo -e "${cW}INFO:${cZ} preparing for build using autotools"
-fi
 
 function show_help
 {
@@ -46,8 +36,8 @@ function show_help
 	echo -e "\t${cW}-r${cZ}"
 	echo -e "\t  Revives the repositories previously packaged with ${cW}-p${cZ}. Implies ${cW}-c${cZ}."
 	echo -e "\t  ${cW}NB:${cZ} to be called from the directory which contains ${MEANDMYSELF}."
-	echo -e "\t${cW}-t${cZ} <targets> (default=$TARGETS)"
-	echo -e "\t  Specify the target architectures for LLVM/Clang (such as x86, x86_64 ...)."
+	echo -e "\t${cW}-t${cZ} <targets> (default=Native)"
+	echo -e "\t  Specify the target architectures for LLVM/Clang."
 	echo -e "\t${cW}-v${cZ}"
 	echo -e "\t  Be verbose about the actions (lines get leading '${cB}[DBG]${cZ}' string)."
 	echo ""
@@ -77,10 +67,13 @@ function prepare_src
 		((REVIVEPKG)) && ( cd "$BASEDIR/$PRJNAME" && echo -ne "\tHard-resetting ($(git config --get core.bare)) after thawing it.\n\t-> "; git reset --hard )
 		# Scrub the working copy
 		((VERBOSE)) && echo -e "${cB}[DBG:$PRJ]${cZ} Cleaning extraneous files from Git clone."
-		( cd "$BASEDIR/$PRJNAME" && git clean -d -f ) || { echo -e "${cR}ERROR:${cZ} failed to 'git clean' $PRJNAME."; exit 1; }
+		( cd "$BASEDIR/$PRJNAME" && git clean -d -f -x) || { echo -e "${cR}ERROR:${cZ} failed to 'git clean -d -f -x' $PRJNAME."; exit 1; }
 		# Get latest changes to the Git repo
 		((VERBOSE)) && echo -e "${cB}[DBG:$PRJ]${cZ} Fetching updates from upstream."
 		( cd "$BASEDIR/$PRJNAME" && git fetch ) || { echo -e "${cY}WARNING:${cZ} failed to 'git fetch' $PRJNAME."; }
+		# Prune branches
+		((VERBOSE)) && echo -e "${cB}[DBG:$PRJ]${cZ} Pruning remote branches."
+		( cd "$BASEDIR/$PRJNAME" && git remote prune origin ) || { echo -e "${cY}WARNING:${cZ} failed to 'git remote prune origin' $PRJNAME."; }
 		# Check out the release
 		((VERBOSE)) && echo -e "${cB}[DBG:$PRJ]${cZ} Checking out the files on current branch."
 		( cd "$BASEDIR/$PRJNAME" && echo -en "${cW}$PRJ:${cZ} " && git checkout -f $GITREF ) || { echo -e "${cR}ERROR:${cZ} failed to check out $GITREF for $PRJNAME."; exit 1; }
@@ -153,17 +146,30 @@ while getopts "h?BcCgGi:Oprt:v" opt; do
 		;;
 	esac
 done
+if ((${LLVM_RELEASE##*_} > 36)); then
+	if ((ONLYCHECKOUT == 0)); then
+		echo -e "${cW}INFO:${cZ} preparing for build using CMake"
+		for tool in cmake; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
+		let CMAKE_BUILD=1
+	fi
+	TARGETS="Native"
+else
+	if ((ONLYCHECKOUT == 0)); then
+		let CMAKE_BUILD=0
+		echo -e "${cW}INFO:${cZ} preparing for build using autotools"
+	fi
+	TARGETS="x86,x86_64"
+fi
 echo -e "\tWorking directory: $BASEDIR"
 let PM=$(grep -c processor /proc/cpuinfo)
 ((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Number of parallel jobs, based on processor count: $PM."
 let TIME_START=$(date +%s)
 ((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Preparing core projects."
 # Preparational steps
-for i in "llvm:git clone http://llvm.org/git/llvm.git" "llvm/tools/clang:git clone http://llvm.org/git/clang.git" "llvm/projects/compiler-rt:git clone http://llvm.org/git/compiler-rt.git" "llvm/projects/libcxx:git clone http://llvm.org/git/libcxx.git" "llvm/tools/clang/tools/extra:git clone http://llvm.org/git/clang-tools-extra.git"; do
+for i in "llvm:git clone http://llvm.org/git/llvm.git" "llvm/tools/clang:git clone http://llvm.org/git/clang.git" "llvm/projects/compiler-rt:git clone http://llvm.org/git/compiler-rt.git" "llvm/projects/libcxx:git clone http://llvm.org/git/libcxx.git" "llvm/projects/libcxxabi:git clone http://llvm.org/git/libcxxabi.git" "llvm/tools/clang/tools/extra:git clone http://llvm.org/git/clang-tools-extra.git"; do
 	prepare_src "$i" "$LLVM_RELEASE"
 done
 ((VERBOSE)) && echo -e "${cB}[DBG]${cZ} Preparing third-party projects."
-prepare_src "llvm/projects/libcxxrt:git clone https://github.com/pathscale/libcxxrt" "$LIBCXXRT_RELEASE"
 if ((NOOPTIONAL == 0)); then
 	prepare_src "3rdparty/binutils:git clone git://sourceware.org/git/binutils-gdb.git" "$BINUTILS_RELEASE"
 fi
@@ -173,7 +179,7 @@ if ((ONLYCHECKOUT==0)) && ((NOBUILD==0)); then
 	if [[ -d "$BASEDIR/build/llvm" ]]; then
 		pushd "$BASEDIR/build/llvm" || { echo -e "${cR}ERROR:${cZ} could not ${cW}pushd${cZ}."; exit 1; }
 		if (( CMAKE_BUILD )); then
-			( set -x; cmake ${TRACE:+--trace} -DCMAKE_INSTALL_PREFIX="$INSTALL_TO" -DLLVM_BUILD_DOCS=OFF -DLLVM_ENABLE_DOXYGEN=OFF -DCMAKE_BUILD_TYPE="Release" -DLLVM_TARGETS_TO_BUILD="${TARGETS//,/;}" "$BASEDIR/llvm" ) || { echo -e "${cR}ERROR:${cZ}  ${cW}cmake${cZ} failed."; exit 1; }
+			( set -x; cmake ${TRACE:+--trace} -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$INSTALL_TO" -DLLVM_BUILD_DOCS=OFF -DLLVM_ENABLE_DOXYGEN=OFF -DCMAKE_BUILD_TYPE="Release" -DLLVM_TARGETS_TO_BUILD="${TARGETS//,/;}" "$BASEDIR/llvm" ) || { echo -e "${cR}ERROR:${cZ}  ${cW}cmake${cZ} failed."; exit 1; }
 		else
 			( set -x; "$BASEDIR/llvm/configure" --prefix=$INSTALL_TO --disable-docs --disable-doxygen --enable-optimized --enable-targets=$TARGETS ) || { echo -e "${cR}ERROR:${cZ}  ${cW}./configure${cZ} failed."; exit 1; }
 		fi
@@ -182,9 +188,6 @@ if ((ONLYCHECKOUT==0)) && ((NOBUILD==0)); then
 		let TIME_MAKE=$(date +%s)
 		( set -x; make -j$PM ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 ${DBG:+TOOL_VERBOSE=1} install )  || { echo -e "${cR}ERROR:${cZ}  ${cW}make install${cZ} failed."; exit 1; }
 		popd || { echo -e "${cR}ERROR:${cZ} could not ${cY}popd${cZ}."; exit 1; }
-		for i in scan-view scan-build; do
-			cp -r "$BASEDIR/llvm/tools/clang/tools/$i"/*  "$INSTALL_TO/bin/"/ || { echo -e "${cY}WARNING:${cZ} could not copy $i binaries/scripts."; }
-		done
 		let TIME_INSTALL=$(date +%s)
 		if ((NOOPTIONAL == 0)); then
 			pushd "$BASEDIR/3rdparty/binutils" || { echo -e "${cR}ERROR:${cZ} could not ${cW}pushd${cZ}."; exit 1; }
@@ -222,7 +225,11 @@ if ((PACKAGEGITGZ==1)); then
 fi
 let TIME_END=$(date +%s)
 if ((ONLYCHECKOUT==0)) && ((NOBUILD==0)); then
-	show_time_diff $TIME_GIT $TIME_CONFIGURE    "./configure took:    %s"
+	if (( CMAKE_BUILD )); then
+		show_time_diff $TIME_GIT $TIME_CONFIGURE    "CMake       took:    %s"
+	else
+		show_time_diff $TIME_GIT $TIME_CONFIGURE    "./configure took:    %s"
+	fi
 	show_time_diff $TIME_CONFIGURE $TIME_MAKE   "GNU make took:       %s"
 	show_time_diff $TIME_MAKE $TIME_INSTALL     "Installation took:   %s"
 	show_time_diff $TIME_INSTALL $TIME_BINUTILS "Binutils took:       %s"
