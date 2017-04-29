32f290be1b6bc11feb0d69e8fe556fa24cd2e4af
Oliver Schneider <oliver@assarbad.net>
2017-04-29 20:32:53 +0000

    Darn, picked the wrong one

--- ollisign.cmd
+++ ollisign.cmd
@@ -30,7 +30,7 @@ if "%~1" == "" goto :NoFileToSign
 if not "%VCVER_FRIENDLY%" == "" @(
   echo Using %VCVER_FRIENDLY%
 )
-set TIMESTAMPSHA1=/tr "http://timestamp.verisign.com/scripts/timstamp.dll"
+set TIMESTAMPSHA1=/tr "http://sha1timestamp.ws.symantec.com/sha1/timestamp"
 set TIMESTAMPSHA2=/tr "http://sha256timestamp.ws.symantec.com/sha256/timestamp" /td sha256 /as
 set IDENTIFIER=/i Symantec%AC%
 if not "%~2" == "" @(
