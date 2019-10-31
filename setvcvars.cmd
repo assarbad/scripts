079684a57d0854e19c109d0a3629f7b64c71edad
Oliver Schneider <oliver@assarbad.net>
2019-10-31 22:43:40 +0100

    Introducing use of vswhere.exe for 2017 and newer. This raises an issue as the (documented) way to specify the -version argument is rather limiting in the scope of the command processor where parentheses and square brackets don't seem to play well with the backtick notation inside 'for'. However, using the old single quote method has the same issue, so it's not specific to the backticks.
    Also updated the test script accordingly.

--- setvcvars.cmd
+++ setvcvars.cmd
@@ -1,12 +1,12 @@
 @echo off
 @if not "%OS%"=="Windows_NT" @(echo This script requires Windows NT 4.0 or later to run properly! & goto :EOF)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::: 2009-2018, Oliver Schneider (assarbad.net) - PUBLIC DOMAIN/CC0
+::: 2009-2019, Oliver Schneider (assarbad.net) - PUBLIC DOMAIN/CC0
 ::: Available from: <https://bitbucket.org/assarbad/scripts/>
 :::
 ::: PURPOSE:    This script can be used to run the vcvars32.bat/vcvarsall.bat
 :::             from any of the existing Visual C++ versions starting with .NET
-:::             (2002) through 2017 or versions (or a single version) given on
+:::             (2002) through 2019 or versions (or a single version) given on
 :::             the command line.
 :::             The script will try to find the newest installed VC version by
 :::             iterating over the space-separated (descending) list of versions
@@ -91,11 +91,12 @@ goto :EOF
 setlocal ENABLEEXTENSIONS & set VCVER=%~1
 :: We're not interested in overwriting an already existing value
 if defined VCVARS_PATH @( endlocal & goto :EOF )
-:: Now let's distinguish the "nice" version numbers (2002, ... 2017) from the internal ones
+:: Now let's distinguish the "nice" version numbers (2002, ... 2019) from the internal ones
 set VCVER=%VCVER:vs=%
-:: Not a "real" version number, but the marketing one (2002, ... 2017)?
+:: Not a "real" version number, but the marketing one (2002, ... 2019)?
 if %VCVER% GEQ %MIN_NICE% call :NICE_%VCVER% > NUL 2>&1
 set NUMVER=%VCVER:.0=%
+set NUMVER=%NUMVER:.1=%
 :: Figure out the set of supported toolsets
 set VCVERLBL=%VCVER:.=_%
 call :PRETTY_%VCVERLBL% > NUL 2>&1
@@ -135,10 +136,12 @@ goto :NICE_SET
     goto :EOF
 :NICE_2019
     set VCVER=16.0
+    set VSWHERE_RANGE=16.0,17.0
     set NEWVS=1
     goto :EOF
 :NICE_2017
     set VCVER=15.0
+    set VSWHERE_RANGE=15.0,16.0
     set NEWVS=1
     goto :EOF
 :NICE_2015
@@ -183,8 +186,20 @@ goto :NICE_SET
 echo Trying to locate Visual C++ %NICEVER% ^(= %VCVER%, %VCTGT_TOOLSET%^)
 :: Is it a version below 15? Then we use the old registry keys
 if %NUMVER% LSS 15 goto :OLDVS
-:: echo Modern (^>=2017) Visual Studio
+echo Modern (^>=2017) Visual Studio
 :: This is where we intend to find the installation path in the registry
+set _VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+"%_VSWHERE%" -? > NUL 2>&1 || set _VSWHERE=vswhere.exe
+"%_VSWHERE%" -? > NUL 2>&1 || goto :SKIPVSWHERE
+::%VSWHERE_RANGE%
+if not defined _VCINSTALLDIR @(
+  for /f "usebackq tokens=*" %%i in (`"%_VSWHERE%" -products * -format value -property installationPath -version %NUMVER%`) do (
+    call :SetVar _VCINSTALLDIR "%%i"
+  )
+)
+if defined _VCINSTALLDIR goto :DETECTION_FINISHED
+:SKIPVSWHERE
+if not "%NICEVER%" == "2017" goto :DETECTION_FINISHED
 set _VSINSTALLKEY=HKLM\SOFTWARE\Microsoft\VisualStudio\SxS\VS7
 if not defined _VCINSTALLDIR @(
   for /f "tokens=2*" %%i in ('reg query "%_VSINSTALLKEY%" /v %VCVER% 2^> NUL') do @(
--- test_setvcvars.cmd
+++ test_setvcvars.cmd
@@ -1,7 +1,7 @@
 @echo off
 @if not "%OS%"=="Windows_NT" @(echo This script requires Windows NT 4.0 or later to run properly! & goto :EOF)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::: 2015, Oliver Schneider (assarbad.net) - PUBLIC DOMAIN/CC0
+::: 2015-2019, Oliver Schneider (assarbad.net) - PUBLIC DOMAIN/CC0
 ::: Available from: <https://bitbucket.org/assarbad/scripts/>
 :::
 ::: PURPOSE:    This script tests the setvcvars.cmd script.
@@ -12,8 +12,9 @@
 :::             software.
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 cls
-set TESTSTORUN=HelpSwitches VS_nosuch VS2002 VS2003 VS2005 VS2008 VS2010 VS2012 VS2013 VS2015 VS2017 VS2005_amd64 VS2008_amd64 VS2010_amd64 VS2012_amd64 VS2013_amd64 VS2015_amd64 VS2017_amd64 VS2005_ia64 VS2008_ia64 VS2010_ia64 VS2012_ia64 VS2013_ia64 VS2015_ia64 VS2017_ia64
+set TESTSTORUN=HelpSwitches VS_nosuch VS2002 VS2003 VS2005 VS2008 VS2010 VS2012 VS2013 VS2015 VS2017 VS2019 VS2005_amd64 VS2008_amd64 VS2010_amd64 VS2012_amd64 VS2013_amd64 VS2015_amd64 VS2017_amd64 VS2019_amd64 VS2005_ia64 VS2008_ia64 VS2010_ia64 VS2012_ia64 VS2013_ia64 VS2015_ia64 VS2017_ia64 VS2019_ia64
 for %%i in (%TESTSTORUN%) do @(
+  @echo.
   @echo TEST %%i
   prompt $G & call :%%i
   prompt
@@ -23,18 +24,23 @@ for %%i in (%TESTSTORUN%) do @(
 :HelpSwitches
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd -h|findstr /l Syntax:
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd --help|findstr /l Syntax:
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd -?|findstr /l Syntax:
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd /h|findstr /l Syntax:
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd /help|findstr /l Syntax:
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :: call /? will show the help for the call command, so we cannot reasonably test that
@@ -43,118 +49,164 @@ call %~dp0setvcvars.cmd /help|findstr /l Syntax:
 :VS2002
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2002
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2002
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 7.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2003
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2003
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2003
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 7.1
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2005
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2005
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2005
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 8.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2008
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2008
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2008
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 9.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2010
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2010
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2010
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 10.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2012
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2012
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2012
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 11.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2013
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2013
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2013
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 12.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2015
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2015
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2015
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 14.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2017
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2017
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2017
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 15.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@goto :EOF
+:VS2019
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd vs2019
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd 2019
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd 16.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :: Some versions that don't exist
 :VS_nosuch
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2001
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 2001
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 1
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2300
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd vs2300
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd 98
@@ -164,155 +216,225 @@ call %~dp0setvcvars.cmd 98
 :VS2005_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2005
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 2005
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 8.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2008_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2008
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 2008
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 9.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2010_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2010
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 2010
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 10.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2012_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2012
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 2012
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 11.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2013_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2013
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 2013
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 12.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2015_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2015
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 2015
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 14.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2017_amd64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 vs2017
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 2017
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd amd64 15.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@goto :EOF
+:VS2019_amd64
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd amd64 vs2019
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd amd64 2019
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd amd64 16.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :: Checking ia64 toolset on all
 :VS2005_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2005
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 2005
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 8.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2008_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2008
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 2008
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 9.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2010_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2010
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 2010
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 10.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2012_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2012
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 2012
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 11.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2013_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2013
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 2013
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 12.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2015_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2015
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 2015
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 14.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
 :VS2017_ia64
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 vs2017
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 2017
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @setlocal&pushd .&echo on
 call %~dp0setvcvars.cmd ia64 15.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@goto :EOF
+:VS2019_ia64
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd ia64 vs2019
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd ia64 2019
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
+@popd&endlocal
+@setlocal&pushd .&echo on
+call %~dp0setvcvars.cmd ia64 16.0
+if defined VCVER_FRIENDLY echo VCVER_FRIENDLY=%VCVER_FRIENDLY%
 @popd&endlocal
 @goto :EOF
