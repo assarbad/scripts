4be78061dbf2b823386c0e9e6763212058d91150
Oliver Schneider <oliver@assarbad.net>
2019-11-10 22:46:53 +0100

    Small fixups to ollisign.cmd and removing some old stuff

--- codesigning/ollisign.cmd
+++ codesigning/ollisign.cmd
@@ -1,7 +1,9 @@
 @echo off
 @if not "%OS%"=="Windows_NT" @(echo This script requires Windows NT 4.0 or later to run properly! & goto :EOF)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::: 2011/16, Oliver Schneider (assarbad.net) - Released into the PUBLIC DOMAIN.
+::: 2011..19, Oliver Schneider (assarbad.net) - Released into the PUBLIC DOMAIN.
+::: (Alternatively under CC0 in jurisdictions where public domain dedications
+:::  by living individuals are not accepted.)
 :::
 ::: DISCLAIMER: Disclaimer: This software is provided 'as-is', without any
 :::             express or implied warranty. In no event will the author be
@@ -11,7 +13,7 @@
 setlocal ENABLEEXTENSIONS
 :: Look in current directory
 set SIGNTOOL=%~dp0signtool.exe
-set AC= /ac %~dp0MSCV-VSClass3.cer
+set AC= /ac "%~dp0Certum Trusted Network CA.crt"
 :: Check whether the caller wants verbose signtool.exe output
 if "%~1" == "-v"        shift&set VERBOSE=1
 if "%~1" == "--verbose" shift&set VERBOSE=1
@@ -20,19 +22,25 @@ if "%~1" == "/verbose"  shift&set VERBOSE=1
 if "%~1" == "-a"        shift&set APPENDSHA2=1
 if "%~1" == "--append"  shift&set APPENDSHA2=1
 if "%~1" == "/a"        shift&set APPENDSHA2=1
+if "%~1" == "-2"        shift&set SHA2ONLY=1
+if "%~1" == "--sha2"    shift&set SHA2ONLY=1
+if "%~1" == "/2"        shift&set SHA2ONLY=1
 if "%~1" == "" goto :NoFileToSign
-:: Check if that succeeds and look for Windows 8.1 SDK (x64) otherwise
-"%SIGNTOOL%" /? >NUL 2>NUL || set SIGNTOOL=%ProgramFiles(x86)%\Windows Kits\10\bin\x64\signtool.exe
-:: Check if that succeeds and look for Windows 8.1 SDK (x86) otherwise
-"%SIGNTOOL%" /? >NUL 2>NUL || set SIGNTOOL=%ProgramFiles(x86)%\Windows Kits\10\bin\x86\signtool.exe
-:: Otherwise look for the newest Visual Studio we can get
-"%SIGNTOOL%" /? >NUL 2>NUL || call setvcvars.cmd > NUL 2>&1
+:: Look for the newest Visual Studio we can find
+set VCVER_FRIENDLY=
+"%SIGNTOOL%" /? >NUL 2>NUL || call setvcvars.cmd
+:: > NUL 2>&1
+set SIGNTOOL=signtool.exe
+:: Fallback #1: Check if that succeeds and look for Windows 8.1 SDK (x64) otherwise
+"%SIGNTOOL%" /? >NUL 2>NUL || set SIGNTOOL=%ProgramFiles(x86)%\Windows Kits\8.1\bin\x64\signtool.exe
+:: Fallback #2: Check if that succeeds and look for Windows 8.1 SDK (x86) otherwise
+"%SIGNTOOL%" /? >NUL 2>NUL || set SIGNTOOL=%ProgramFiles(x86)%\Windows Kits\8.1\bin\x86\signtool.exe
 if not "%VCVER_FRIENDLY%" == "" @(
   echo Using %VCVER_FRIENDLY%
 )
-set TIMESTAMPSHA1=/tr "http://timestamp.digicert.com"
-set TIMESTAMPSHA2=/tr "http://timestamp.digicert.com" /td sha256 /as
-set IDENTIFIER=/i Symantec%AC%
+set TIMESTAMPSHA1=/tr "http://time.certum.pl"
+set TIMESTAMPSHA2=/tr "http://timestamp.digicert.com" /td sha256
+set IDENTIFIER=/i Certum
 if not "%~2" == "" @(
   call :SetVar DESCRIPTURL "%~2"
 )
@@ -40,14 +48,18 @@ if not "%~3" == "" @(
   call :SetVar DESCRIPTION "%~3"
 )
 set VRFYCMD="%SIGNTOOL%" verify
-set SIGNCMD="%SIGNTOOL%" sign /a %IDENTIFIER% /ph
+set SIGNCMD="%SIGNTOOL%" sign /v /debug /a %IDENTIFIER% /ph
 if not "%VERBOSE%" == "" set SIGNCMD=%SIGNCMD% /v /debug
 if not "%DESCRIPTURL%" == "" set SIGNCMD=%SIGNCMD% /du "%DESCRIPTURL%"
 if not "%DESCRIPTION%" == "" set SIGNCMD=%SIGNCMD% /d "%DESCRIPTION%"
 set SIGNCMD=%SIGNCMD%
+echo SHA2ONLY=%SHA2ONLY%
+if not defined SHA2ONLY set APPENDSIG= /as
 :: Now sign ...
-echo %SIGNCMD% %TIMESTAMPSHA1% "%~1"&%SIGNCMD% %TIMESTAMPSHA1% "%~1"
-if "%APPENDSHA2%" == "1" echo %SIGNCMD% /fd sha256 %TIMESTAMPSHA2% "%~1"&%SIGNCMD% /fd sha256 %TIMESTAMPSHA2% "%~1"
+@echo on
+if not "%SHA2ONLY%" == "1" (echo %SIGNCMD% %TIMESTAMPSHA1% "%~1"&%SIGNCMD% %TIMESTAMPSHA1% "%~1")
+if not "%APPENDSHA2%%SHA2ONLY%" == "" (echo %SIGNCMD% /fd sha256 %TIMESTAMPSHA2%%APPENDSIG% "%~1"&%SIGNCMD% /fd sha256 %TIMESTAMPSHA2%%APPENDSIG% "%~1")
+@echo off
 :: And verify
 if "%APPENDSHA2%" == "1" %VRFYCMD% /pa /all "%~1"
 if not "%APPENDSHA2%" == "1" %VRFYCMD% /kp "%~1"
