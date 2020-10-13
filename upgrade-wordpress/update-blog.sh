5b7527aabf0478e78f5b342bb92a0f4dec66865d
Oliver Schneider <oliver@assarbad.net>
2020-10-14 00:09:25 +0200

    New script plus minor updates
    - New script to build some statically linked versions of useful
      and/or interesting Rust CLI tools
    - Moving to latest release tag for Clang/LLVM and binutils

--- upgrade-wordpress/update-blog.sh
+++ upgrade-wordpress/update-blog.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vim: set autoindent smartindent tabstop=4 shiftwidth=4 noexpandtab filetype=sh:
 [[ -t 1 ]] && { cG="\033[1;32m"; cR="\033[1;31m"; cB="\033[1;34m"; cW="\033[1;37m"; cY="\033[1;33m"; cG_="\033[0;32m"; cR_="\033[0;31m"; cB_="\033[0;34m"; cW_="\033[0;37m"; cY_="\033[0;33m"; cZ="\033[0m"; export cR cG cB cY cW cR_ cG_ cB_ cY_ cW_ cZ; }
-for tool in find grep mysqldump perl sed sha1sum sudo tar wc wget; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
+for tool in find grep mktemp mysqldump perl sed sha1sum sudo tar wc wget; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
 [[ -n "$DEBUG" ]] && set -x
 pushd $(dirname $0) > /dev/null; CURRABSPATH=$(readlink -nf "$(pwd)"); popd > /dev/null; # Get the directory in which the script resides
 
