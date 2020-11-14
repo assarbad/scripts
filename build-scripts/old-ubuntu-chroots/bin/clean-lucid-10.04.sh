40773cd5e51b39fb42fd868df19b5aad43a75ee2
Oliver Schneider <oliver@assarbad.net>
2020-11-14 01:02:44 +0100

    Turns out three of those would get reinstalled with the prepare script anyway

--- build-scripts/old-ubuntu-chroots/bin/clean-lucid-10.04.sh
+++ build-scripts/old-ubuntu-chroots/bin/clean-lucid-10.04.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 apt-mark unmarkauto bc
 apt-get -f autoremove linux-virtual linux-image-ec2 linux-ec2 linux-image-2.6.32-73-virtual linux-image-2.6.32-376-ec2 memtest86+ grub-legacy-ec2 grub-pc grub-common
-apt-get -f autoremove command-not-found command-not-found-data cloud-init cloud-utils dosfstools e2fslibs e2fsprogs euca2ools friendly-recovery laptop-detect ntfs-3g openssh-client openssh-server parted pciutils unattended-upgrades update-motd wpasupplicant wireless-tools ureadahead tasksel tasksel-data ssh-import lshw iptables update-manager
+apt-get -f autoremove command-not-found command-not-found-data cloud-init cloud-utils dosfstools euca2ools friendly-recovery laptop-detect ntfs-3g openssh-server parted pciutils unattended-upgrades update-motd wpasupplicant wireless-tools ureadahead tasksel tasksel-data ssh-import lshw iptables update-manager
 aptitude purge $(dpkg -l|awk '$1 ~ /^rc/ {print $2}')
-rm -rf /root/.aptitude /root/.debtags /etc/
-rm -f /root/.viminfo
 apt-get -y clean
 apt-get -y autoclean
 aptitude -y clean
@@ -14,3 +12,5 @@ for i in /etc/.pwd.lock /var/log/*.{0..9} /var/log/*.{0..9}.gz /var/cache/apt/ar
 	[[ -e "$i" ]] && (set -x; rm -f "$i")
 done
 find /etc -type f -name '*.dpkg-old' -delete
+rm -rf /root/.aptitude /root/.debtags /etc/ssh /etc/cloud
+rm -f /root/.viminfo
