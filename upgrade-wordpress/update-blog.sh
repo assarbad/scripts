9fb4e11c416ee9c8eb9d7e7571874f50814e01c1
Oliver Schneider <oliver@assarbad.net>
2022-10-06 23:36:57 +0200

    Updating the blog update script

--- upgrade-wordpress/update-blog.sh
+++ upgrade-wordpress/update-blog.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vim: set autoindent smartindent tabstop=4 shiftwidth=4 noexpandtab filetype=sh:
 [[ -t 1 ]] && { cG="\033[1;32m"; cR="\033[1;31m"; cB="\033[1;34m"; cW="\033[1;37m"; cY="\033[1;33m"; cG_="\033[0;32m"; cR_="\033[0;31m"; cB_="\033[0;34m"; cW_="\033[0;37m"; cY_="\033[0;33m"; cZ="\033[0m"; export cR cG cB cY cW cR_ cG_ cB_ cY_ cW_ cZ; }
-for tool in find grep mysqldump perl sed sha1sum sudo tar wc wget; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
+for tool in find grep mysqldump perl sed sha1sum sha256sum sudo tar tee wc wget; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
 [[ -n "$DEBUG" ]] && set -x
 pushd $(dirname $0) > /dev/null; CURRABSPATH=$(readlink -nf "$(pwd)"); popd > /dev/null; # Get the directory in which the script resides
 
@@ -64,8 +64,9 @@ function blog_update
 		&& \
 		sudo mysqldump --defaults-file=/etc/mysql/debian.cnf --ignore-table=${DB_NAME}.wp_comments --opt "$DB_NAME" \
 		) > "$OLDDBBKUP"; then
+		( set -x; ( cd "${OLDDBBKUP%/*}" && sha256sum "${OLDDBBKUP##*/}" )|tee "$OLDDBBKUP.SHA256SUM" )
 		echo -e "Backing up ${cW}folder${cZ}"
-		if ( set -x; tar -C "$BASEDIR" -czf "$OLDWPBKUP" "${BLOGBASE#$BASEDIR/}" "${WPCONFIG#$BASEDIR/}" "${OLDDBBKUP##*/}" ); then
+		if ( set -x; tar -C "$BASEDIR" -czf "$OLDWPBKUP" "${BLOGBASE#$BASEDIR/}" "${WPCONFIG#$BASEDIR/}" $(cd "$BASEDIR" && find -type f -maxdepth 1 -name 'update-blog.sh' -o -name 'update-wp-*' -o -name '.update-blog.*' -o -name '.update-wp-*') "${OLDDBBKUP##*/}" && ( cd "${OLDWPBKUP%/*}" && sha256sum "${OLDWPBKUP##*/}" )|tee "$OLDWPBKUP.SHA256SUM" ); then
 			if [[ "$UPDTGZ" != "--backup" ]] && [[ "$UPDTGZ" != "-b" ]]; then
 				echo -e "Removing old wp-include and wp-admin"
 				rm -rf "$BLOGBASE/wp-includes" "$BLOGBASE/wp-admin" || \
@@ -84,16 +85,16 @@ function blog_update
 				fi
 			else
 				echo -e "${cW}INFO:${cZ} no new release tarball given, skipping actual upgrade."
-				rm -f "$OLDDBBKUP"
+				rm -f "$OLDDBBKUP" "$OLDDBBKUP.SHA256SUM"
 				echo -e "Done."
 			fi
 		else
-			rm -f "$OLDDBBKUP" "$OLDWPBKUP" 2> /dev/null
+			rm -f "$OLDDBBKUP" "$OLDWPBKUP" "$OLDDBBKUP.SHA256SUM" "$OLDWPBKUP.SHA256SUM" 2> /dev/null
 			echo -e "${cR}ERROR${cZ}: could not back up old WordPress folder"
 			exit 1
 		fi
 	else
-			rm -f "$OLDDBBKUP" "$OLDWPBKUP" 2> /dev/null
+			rm -f "$OLDDBBKUP" "$OLDWPBKUP" "$OLDDBBKUP.SHA256SUM" "$OLDWPBKUP.SHA256SUM" 2> /dev/null
 			echo -e "${cR}ERROR${cZ}: could not back up old WordPress database"
 			exit 1
 	fi
