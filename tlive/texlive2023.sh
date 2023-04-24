4aca6f04acd0f5c1743faecd9460ae48975e25b2
Oliver Schneider <oliver@assarbad.net>
2023-04-24 21:28:02 +0200

    Adjusting for TeXlive 2023

--- tlive/texlive2023.sh
+++ tlive/texlive2023.sh
@@ -3,7 +3,7 @@
 ((${BASH_VERSION%%.*} >= 4)) || { echo -e "${cR}ERROR:${cZ}: expecting a minimum Bash version of 4. You have ${BASH_VERSION}."; exit 1; }
 for tool in rsync sha256sum tar tee; do type $tool > /dev/null 2>&1 || { echo -e "${cR}ERROR:${cZ} couldn't find '$tool' which is required by this script."; exit 1; }; done
 pushd $(dirname $0) > /dev/null; CURRABSPATH=$(readlink -nf "$(pwd)"); popd > /dev/null; # Get the directory in which the script resides
-TGTDIR="$CURRABSPATH/texlive2022"
+TGTDIR="$CURRABSPATH/texlive2023"
 if [[ -d "$TGTDIR" ]]; then
 	(set -x; rsync -avP --delete-after rsync://rsync.dante.ctan.org/CTAN/systems/texlive/tlnet/ "$TGTDIR/") || { echo -e "${cR}ERROR:${cZ} ${cW}rsync${cZ} failed!"; exit 1; }
 	(set -x; cd "$CURRABSPATH"; tar -cf "${TGTDIR##*/}.tar" "${0##*/}" "${TGTDIR##*/}" && sha256sum "${TGTDIR##*/}.tar"|tee "$TGTDIR.tar.SHA256SUM") || { echo -e "${cR}ERROR:${cZ} ${cW}tar${cZ} failed!"; exit 1; }
